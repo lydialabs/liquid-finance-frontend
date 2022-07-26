5df5ad1414148227c81f8d236442cc8bea773c64
NgaNTT <cowslips9@gmail.com>
2022-07-26 23:11:04 +0700

    fix: update new address contract

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -1,8 +1,8 @@
 export const isProduction = process.env.NODE_ENV === "production";
 export const isDevelopment = process.env.NODE_ENV === "development";
 export const CONTRACT_ADDRESS = {
-  staking: "archway1nqm0lq09thzq9c8ryrnsmryeavr8r2ph4qufpcun0ntlpzn5j7qqyvswe3",
-  larch: "archway12m9xkxn9n3zzl2n2vjv7rfll8pujy37wk7mhy5tj4fnxyxet67gqp8n7lu",
-  swap: "archway1lreel8jtntxev6klzjdk2gk77nns9s7pqr7x4l4erpf82e97dskswx0t2y",
+  staking: "archway1hjcvxnu6sr96lta740wvt3h2vgmce6mwe4achs5uejf9hgj7m6rqyaz9yh",
+  larch: "archway16mvu3cta6nu8333zwt87ldnnawvyev6txgehjy66uwnkxg0p6lhs5cx9jl",
+  swap: "archway1n90z98gs2h42q73hc5fpm6yzqgv2dktk5meqye783hpafzqsyvjsxx6dm9",
   native: "archwayX00000000000000000000000000000000000000000000000000000000",
 };
